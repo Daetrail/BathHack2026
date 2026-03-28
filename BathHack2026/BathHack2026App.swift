64c9079b2837cf41226c402d79c35034c775d132
Matt Dustin Cruz <cruzmattdustin@gmail.com>
2026-03-28 10:11:05 +0000

    Initial Commit

---
+++ BathHack2026/BathHack2026App.swift
@@ -0,0 +1,17 @@
+//
+//  BathHack2026App.swift
+//  BathHack2026
+//
+//  Created by Matt Dustin Cruz on 28/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct BathHack2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BathHack2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BathHack2026
+//
+//  Created by Matt Dustin Cruz on 28/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
